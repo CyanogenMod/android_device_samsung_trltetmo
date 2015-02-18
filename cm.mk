59aeaa49bbc38ecdaca7bbbb74a2345904a6c2b2
blhf <blhfestin@gmail.com>
2015-02-18 10:22:02 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/samsung/trltetmo/full_trltetmo.mk)
+$(call inherit-product, device/samsung/lentisltelgt/full_lentisltelgt.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -22,5 +22,5 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit common CM phone.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_DEVICE := trltetmo
-PRODUCT_NAME := cm_trltetmo
+PRODUCT_DEVICE := lentisltelgt
+PRODUCT_NAME := cm_lentisltelgt
